09f3251b930dc65d047e663104dd4e0e2dd294c9
yamiltame <yamiltamesoria@gmail.com>
2019-10-16 19:40:54 -0500

    first commit

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "1220";		     // database password
+$dbname	= "Base1";    // database name
+?>
